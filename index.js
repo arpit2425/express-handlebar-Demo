83372e40e69d1622749bd5dfad43265fc884b345
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-01-01 23:45:25 +0530

    Loop an array

---
+++ index.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const ehb=require('express-handlebars');
+const app=express();
+const path=require('path');
+
+app.engine("hbs",ehb({
+    defaultLayout:"mainlayout",
+    extname:'hbs',
+    layoutsDir:path.join(__dirname,"views/help")
+}));
+app.set("view engine", "hbs");
+app.get("/",(req,res)=>{
+    res.render("index",{title:"Home page",
+    isdisplay:false,
+    about:"fdnjkjfvvfnjfj"});
+})
+app.get("/dash",(req,res)=>{
+    res.render("dashboard",{list:true})
+})
+app.get("/loop",(req,res)=>{
+    res.render("loop",{
+        people:[
+            "Arpit",
+            "fejknj",
+            "vjnjv"
+        ]
+    })
+})
+app.listen(3000,()=>{
+    console.log("Success");
+})
